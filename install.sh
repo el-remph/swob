49e52a04e11a63e5db2e36f64ec87c2109172db9
remph <lhr@disroot.org>
2024-12-18 19:12:45 +0000

    Support installation under /usr/local
    Use `PREFIX=/usr/local ETCDIR=$PREFIX/etc ./install.sh'

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -ex
-mkdir -p "$DESTDIR"/etc/sway/config.d/ "$DESTDIR"/etc/swob/
+: ${PREFIX=/usr} ${ETCDIR:=/etc}
+
 for i in swayconfig wob.ini; do
-	install -m 644 swob-$i "$DESTDIR"/etc/swob/$i
+	install -Dm 644 swob-$i "$DESTDIR$ETCDIR"/swob/$i
 done
-ln -s ../../swob/swayconfig "$DESTDIR"/etc/sway/config.d/swob-swayconfig
-install -Dt "$DESTDIR"/usr/bin swob.sh
-install -m644 -Dt "$DESTDIR"/usr/share/doc/swob README.md dwl-config.h.patch
+
+install -d "$DESTDIR$ETCDIR"/sway/config.d/
+ln -s ../../swob/swayconfig "$DESTDIR$ETCDIR"/sway/config.d/swob-swayconfig
+
+install -Dt "$DESTDIR$PREFIX"/bin swob.sh
+install -m644 -Dt "$DESTDIR$PREFIX"/share/doc/swob README.md dwl-config.h.patch
